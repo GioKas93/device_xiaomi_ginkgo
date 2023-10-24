c42af876194d971b98c604566b9a50c417197905
GioKas93 <giokas93@pm.me>
2023-10-24 14:34:52 +0200

    BLKI bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/blackiron_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    blackiron_ginkgo-user \
+    blackiron_ginkgo-userdebug \
+    blackiron_ginkgo-eng
